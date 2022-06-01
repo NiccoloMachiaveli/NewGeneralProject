bc623b846d5deb8dfdd2ee8a41faa78ebd2f4b72
SuperPusya <106535682+SuperPusya@users.noreply.github.com>
2022-06-01 12:38:25 +0300

    1

---
+++ GGG/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GGG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
